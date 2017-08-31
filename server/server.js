2068da3a464962bc8da4c019a3b16f56dd6ccf4b
Joel Guizar <joelguizar@gmail.com>
2017-08-31 09:44:13 -0700

    req.params.id

--- server/server.js
+++ server/server.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const bodyParser = require('body-parser'); //takes your JSON and turns it into an Obj
 const {mongoose} = require('./db/mongoose.js');
-var {Todo} = require ('./models/todo.js');
+const {Todo} = require ('./models/todo.js');
 const {User} = require('./models/user.js');
+const {ObjectId} = require('mongodb');
 
 const app = express();
 const PORT = 3000;
@@ -30,6 +31,22 @@ app.get('/todos', (req, res) => {
   Todo.find().then((todos) => {
     res.send({todos}) // don't just send an array, by putting it in an object you are more flexible to add more properties to the response
   }, (e) => {res.status(400).send(e)})
+})
+
+app.get('/todos/:id', (req, res) => {
+  let id = req.params.id //will take the params.id from the request body, which is the :id
+    if (!ObjectId.isValid(id)){
+      res.status(404).send();
+      return console.log('LOL this doesnt exist');
+    }
+
+    Todo.findById(id).then((docs) => {
+      if (docs){
+        res.status(200).send(docs);
+        return console.log('SUCCESS!!!', docs);
+      }
+      res.send(404)
+    }).catch(e => {res.status(400).send()})
 })
 
 
