6530d13ab6914fe25559d99298cac3d1508c8bfa
Joel Guizar <joelguizar@gmail.com>
2017-09-12 23:17:22 -0700

    it should create a user test

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -165,6 +165,38 @@ describe('GET /users/me', () => {
   })
 
   it('should return 401 if not authenticated', (done) =>{
+    request(app)
+      .get('/users/me')
+      .expect(401)
+      .expect((res) => {
+        expect(res.body).toEqual({})
+      })
+      .end(done)
+  })
+})
+
+describe('POST /users', () => {
+  it('it should create a user', (done) => {
+    let email = 'email@email.com';
+    let pw = '123abc';
+
+    request(app)
+      .post('/users')
+      .send({email, password})
+      .expect(200)
+      .expect((res) => {
+        expect(res.headers['x-auth']).toExist(); //we use [] notation because there's a hyphen
+        expect(res.body._id).toExist();
+        expect(res.body.email).toBe(email);
+      })
+      .end(done)
+  })
+
+  it('should return validation errors if request invalid', (done) => {
+
+  });
+
+  it('should not create user if email in use', (done) => {
 
   })
 })
